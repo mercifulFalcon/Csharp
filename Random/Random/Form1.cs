94858285da38131256d86e7cbe7550025f1158ff
mercifulFalcon <regisezequiel@yahoo.com>
2025-09-06 16:06:28 -0300

    Agragada restricción números iguales

--- Random/Random/Form1.cs
+++ Random/Random/Form1.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Diagnostics.Eventing.Reader;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -25,10 +26,13 @@ namespace Random
         {
             int num1 = int.Parse(textBox1.Text);
             int num2 = int.Parse(textBox2.Text);
-
-            int elegido = (rnd.Next(2) == 0) ? num1 : num2;
+            if (num1 == num2)
+            { MessageBox.Show("Los números son iguales!!"); }
+            else{ 
+                int elegido = (rnd.Next(2) == 0) ? num1 : num2;
 
             label1.Text = $" {elegido}";
+            }
         }
     }
 }
