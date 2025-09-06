9fc06ecdf805673d47366cc24477f63e83fdd856
mercifulFalcon <regisezequiel@yahoo.com>
2025-09-06 18:55:39 -0300

    Terminado

---
+++ Escalera/Escalera/Form1.Designer.cs
@@ -0,0 +1,102 @@
+﻿namespace Escalera
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtMin = new System.Windows.Forms.TextBox();
+            this.txtMax = new System.Windows.Forms.TextBox();
+            this.btnContar = new System.Windows.Forms.Button();
+            this.lstResultados = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // txtMin
+            // 
+            this.txtMin.Font = new System.Drawing.Font("Futura Std Light", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtMin.Location = new System.Drawing.Point(146, 86);
+            this.txtMin.Name = "txtMin";
+            this.txtMin.Size = new System.Drawing.Size(100, 28);
+            this.txtMin.TabIndex = 0;
+            this.txtMin.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // txtMax
+            // 
+            this.txtMax.Font = new System.Drawing.Font("Futura Std Light", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtMax.Location = new System.Drawing.Point(545, 86);
+            this.txtMax.Name = "txtMax";
+            this.txtMax.Size = new System.Drawing.Size(100, 28);
+            this.txtMax.TabIndex = 1;
+            this.txtMax.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // btnContar
+            // 
+            this.btnContar.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(0)))));
+            this.btnContar.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.btnContar.Font = new System.Drawing.Font("Futura Std Light", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnContar.Location = new System.Drawing.Point(349, 186);
+            this.btnContar.Name = "btnContar";
+            this.btnContar.Size = new System.Drawing.Size(80, 28);
+            this.btnContar.TabIndex = 2;
+            this.btnContar.Text = "Escalera";
+            this.btnContar.UseVisualStyleBackColor = false;
+            this.btnContar.Click += new System.EventHandler(this.btnContar_Click);
+            // 
+            // lstResultados
+            // 
+            this.lstResultados.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.lstResultados.FormattingEnabled = true;
+            this.lstResultados.Location = new System.Drawing.Point(328, 283);
+            this.lstResultados.Name = "lstResultados";
+            this.lstResultados.Size = new System.Drawing.Size(120, 95);
+            this.lstResultados.TabIndex = 3;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Firebrick;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lstResultados);
+            this.Controls.Add(this.btnContar);
+            this.Controls.Add(this.txtMax);
+            this.Controls.Add(this.txtMin);
+            this.Name = "Form1";
+            this.Text = "Escalera";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtMin;
+        private System.Windows.Forms.TextBox txtMax;
+        private System.Windows.Forms.Button btnContar;
+        private System.Windows.Forms.ListBox lstResultados;
+    }
+}
+
---
+++ Escalera/Escalera/Form1.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics.Eventing.Reader;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Escalera
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnContar_Click(object sender, EventArgs e)
+        {
+            lstResultados.Items.Clear();
+
+            if (int.TryParse(txtMin.Text, out int min) &&
+                int.TryParse(txtMax.Text, out int max))
+            {
+                if (min < max)
+                {
+                    for (int i = min; i <= max; i++)
+                    {
+                        lstResultados.Items.Add(i.ToString());
+                    }
+                }
+                else if (min > max)
+                {
+                    for (int i = min; i >= max; i--)
+                    {
+                        lstResultados.Items.Add(i.ToString());
+                    }
+                }
+                else
+                {
+                    MessageBox.Show("Los números son iguales");
+                }
+            }
+            else
+            {
+                MessageBox.Show("Algún número es invalido");
+            }
+        }
+    }
+}
