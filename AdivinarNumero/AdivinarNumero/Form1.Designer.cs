70509b1de41316ebf4d5a6b17ace378266a71e92
EzequielRegis <regisezequiel@yahoo.com>
2025-09-13 21:45:03 -0300

    Evaluación 3#

---
+++ AdivinarNumero/AdivinarNumero/Form1.Designer.cs
@@ -0,0 +1,92 @@
+﻿namespace AdivinarNumero
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnComprobar = new System.Windows.Forms.Button();
+            this.lblResultado = new System.Windows.Forms.Label();
+            this.txtNumero = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // btnComprobar
+            // 
+            this.btnComprobar.AutoSize = true;
+            this.btnComprobar.Location = new System.Drawing.Point(216, 116);
+            this.btnComprobar.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.btnComprobar.Name = "btnComprobar";
+            this.btnComprobar.Size = new System.Drawing.Size(89, 35);
+            this.btnComprobar.TabIndex = 0;
+            this.btnComprobar.Text = "ADIVINAR";
+            this.btnComprobar.UseVisualStyleBackColor = true;
+            this.btnComprobar.Click += new System.EventHandler(this.btnComprobar_Click);
+            // 
+            // lblResultado
+            // 
+            this.lblResultado.BackColor = System.Drawing.Color.Firebrick;
+            this.lblResultado.Location = new System.Drawing.Point(146, 180);
+            this.lblResultado.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblResultado.Name = "lblResultado";
+            this.lblResultado.Size = new System.Drawing.Size(228, 20);
+            this.lblResultado.TabIndex = 1;
+            this.lblResultado.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // txtNumero
+            // 
+            this.txtNumero.Location = new System.Drawing.Point(198, 49);
+            this.txtNumero.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.txtNumero.Name = "txtNumero";
+            this.txtNumero.Size = new System.Drawing.Size(132, 28);
+            this.txtNumero.TabIndex = 2;
+            this.txtNumero.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Firebrick;
+            this.ClientSize = new System.Drawing.Size(517, 263);
+            this.Controls.Add(this.txtNumero);
+            this.Controls.Add(this.lblResultado);
+            this.Controls.Add(this.btnComprobar);
+            this.Font = new System.Drawing.Font("Futura Std Light", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.Name = "Form1";
+            this.Text = "Adivina el número entre 1 y 20";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnComprobar;
+        private System.Windows.Forms.Label lblResultado;
+        private System.Windows.Forms.TextBox txtNumero;
+    }
+}
+
---
+++ AdivinarNumero/AdivinarNumero/Form1.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AdivinarNumero
+{
+    public partial class Form1 : Form
+    {
+
+        private int numeroSecreto;
+        private int intentos;
+        private Random random = new Random();
+        public Form1()
+        {
+            InitializeComponent();
+            IniciarJuego();
+
+            
+            this.AcceptButton = btnComprobar;
+        }
+
+        private void IniciarJuego()
+        {
+            numeroSecreto = random.Next(1, 21); 
+            intentos = 0;
+            lblResultado.Text = " ";
+           txtNumero.Clear();
+            txtNumero.Focus();
+        }
+
+        private void btnComprobar_Click(object sender, EventArgs e)
+        {
+            if (int.TryParse(txtNumero.Text, out int intento))
+            {
+                intentos++;
+
+                switch (intento.CompareTo(numeroSecreto))
+                {
+                    case -1:
+                        lblResultado.Text = "El número es mayor.";
+                        break;
+                    case 1:
+                        lblResultado.Text = "El número es menor.";
+                        break;
+                    case 0:
+                        MessageBox.Show($"¡Adivinaste! y lo hiciste en {intentos} intentos");
+                        IniciarJuego();
+                        return;
+                }
+
+                
+                txtNumero.Clear();
+                txtNumero.Focus();
+            }
+            else
+            {
+                MessageBox.Show("Por favor, ingresa un número válido.");
+                
+            }
+                txtNumero.Clear();
+                txtNumero.Focus();
+        }
+    }
+}
